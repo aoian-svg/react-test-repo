7d4d037f2af0cdf05fb3b5213771731be7fa17b3
Andrea Oian <a.oian@reply.it>
2026-01-22 17:35:53 +0100

    First

--- ReactSuperSquad/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import characters from "./charactersReducer";
-import heroes from "./heroesReducers";
-
-const rootReducer = combineReducers({ characters, heroes });
-
-export default rootReducer;
