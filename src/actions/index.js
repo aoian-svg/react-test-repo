6a6f540ed253fef93796a8f220272b0a648442dc
aoian-svg <a.oian@reply.it>
2026-03-04 15:24:57 +0100

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 export const ADD_CHARACTER = "ADD_CHARACTER";
 export const REMOVE_CHARACTER = "REMOVE_CHARACTER";
+export const REMOVE_CHARACTER = "TEST";
 
 export const addCharacterById = id => {
   const action = {
@@ -12,6 +13,14 @@ export const addCharacterById = id => {
 export const removeCharacterById = id => {
   const action = {
     type: REMOVE_CHARACTER,
+    payload: id
+  };
+  return action;
+};
+
+export const test = test => {
+  const action = {
+    type: TEST,
     payload: id
   };
   return action;
